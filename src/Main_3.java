2204d50d3ffe7748ffaa77961e8510f7c0c03292
gordoneza01 <gordoneza01@elpuig.xeill.net>
2022-05-05 17:29:04 +0200

    q

--- src/Main_3.java
+++ src/Main_3.java
@@ -1,19 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main_3 {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int numero_de_valores = scanner.nextInt();
-        String array = "int[] myArray = { ";
-        for (int i = 0; i<numero_de_valores - 1; i++){
-            int n = scanner.nextInt();
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("entrades");
+        File[] files = file.listFiles();
 
-            array += n + ", ";
+         for(File f : files) {
+            FileReader fr = new FileReader(f.getPath());
+            Scanner scanner = new Scanner(fr);
 
+            int numero_de_valores = scanner.nextInt();
+            String array = "int[] myArray = { ";
+
+            for (int j = 0; j < numero_de_valores - 1; j++) { //El - 1 es para decir que no ponga la ultima ","
+                int n = scanner.nextInt();
+
+                array += n + ", ";
+
+            }
+            array += scanner.nextInt() + " };";
+            System.out.println(array);
         }
-        array += scanner.nextInt() + " };";
-        System.out.println(array);
     }
-
 }
